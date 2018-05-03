01f394bd79b922a4c3ba58212bb1660a1039f62c
Derrek Larson <larso077@rangers.uwp.edu>
2018-05-03 11:35:09 -0500

    more documentation

--- Homework Navigator/Homework Navigator/HomeworkViewController.swift
+++ Homework Navigator/Homework Navigator/HomeworkViewController.swift
@@ -4,26 +4,30 @@
 //
 //  Created by Derrek Larson on 4/24/18.
 //  Copyright © 2018 Hoffman Express. All rights reserved.
-//
+//  This is your work zone. each of these delegate methods is required to make the table view work.
 
 import UIKit
 
 class HomeworkViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
 {
+    //this method should return the number of data you wish to represent in your tableview separated by section.
+    //examples of this would be return numberOfAssignmentsInOneClass, with a case switch on the section int
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
     }
-
+    // this one gives you a tableview cell at each row, usually grab the object from your array at objectArray[indexPath], use data
+    //  from that object to fill in the labels and stuff in your tableviewcell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //customize this, this is only to eliminate errors.
         let tableviewcell = UITableViewCell()
         return tableviewcell
     }
+    // this is how
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
     
-
+    //put any data initialization here
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -34,7 +38,7 @@ class HomeworkViewController: UIViewController,UITableViewDelegate,UITableViewDa
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    // add a viewWillAppear method if you need to do something every time this view comes up. should autofill
 
     /*
     // MARK: - Navigation
