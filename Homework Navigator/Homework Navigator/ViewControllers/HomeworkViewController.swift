84aa54a1d672fd05f5188963654bc681b76656c6
Colin Hoffman <c.hoffman2009@yahoo.com>
2018-05-06 15:25:43 -0500

    Finished implementing the home work and submission pages. Now all pages have full function and data populating all tables

--- Homework Navigator/Homework Navigator/ViewControllers/HomeworkViewController.swift
+++ Homework Navigator/Homework Navigator/ViewControllers/HomeworkViewController.swift
@@ -2,53 +2,69 @@
 //  HomeworkViewController.swift
 //  Homework Navigator
 //
-//  Created by Derrek Larson on 4/24/18.
+//  Created by Colin Hoffman on 5/5/18.
 //  Copyright © 2018 Hoffman Express. All rights reserved.
 //  This is your work zone. each of these delegate methods is required to make the table view work.
 
 import UIKit
 
+class HomeworkCell: UITableViewCell {
+    @IBOutlet weak var homeworkLabel: UILabel!
+    @IBOutlet weak var dueDateLabel: UILabel!
+}
+
 class HomeworkViewController: UIViewController
-//    ,UITableViewDelegate,UITableViewDataSource
+    ,UITableViewDelegate,UITableViewDataSource
 {
-//    //this method should return the number of data you wish to represent in your tableview separated by section.
-//    //examples of this would be return numberOfAssignmentsInOneClass, with a case switch on the section int
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 0
-//    }
-//    // this one gives you a tableview cell at each row, usually grab the object from your array at objectArray[indexPath], use data
-//    //  from that object to fill in the labels and stuff in your tableviewcell
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        //customize this, this is only to eliminate errors.
-//        let tableviewcell = UITableViewCell()
-//        return tableviewcell
-//    }
-//    // this is how
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        return 1
-//    }
     
-    //put any data initialization here
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var homeworkTableView: UITableView!
+    
+        let homeworkArray = [[("4-12", "Sudoku"), ("5-1", "LZW")],[("4-22", "Python"), ("5-1", "Prolog"), ("5-4", "Racket")],[("4-20", "Paper/Presentation"), ("5-3", "Cache Quiz"), ("5-3", "Networking Quiz")],[("3-17", "HW3"), ("4-1", "HW4"), ("4-7", "HW5"), ("4-14", "HW6"), ("4-21", "HW7"), ("5-1", "Final Project")]]
+    
+    let classesArray = ["CSCI 340", "CSCI 333", "CSCI 355", "CSCI 423"]
+        
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return classesArray.count;
+        }
+    
+        func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+            guard classesArray.indices ~= section else {
+                print("No section title for this section")
+                return nil
+            }
+            
+            return classesArray[section]
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return homeworkArray[section].count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell  = homeworkTableView.dequeueReusableCell(withIdentifier: "homeworkCell", for: indexPath) as! HomeworkCell
+            
+            cell.dueDateLabel.text = homeworkArray[indexPath.section][indexPath.row].0
+            cell.homeworkLabel.text = homeworkArray[indexPath.section][indexPath.row].1
 
-        // Do any additional setup after loading the view.
-    }
+            return cell
+        }
+    
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            homeworkTableView.delegate = self
+            homeworkTableView.dataSource = self
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    // add a viewWillAppear method if you need to do something every time this view comes up. should autofill
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        }
+        
+        override func viewWillAppear(_ animated: Bool)
+        {
+            self.homeworkTableView.reloadData()
+        }
+        
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
 
 }
